4d6236b63922d081eeaf3b8597c8687d38cc4c06
AshwiniRajashekar <ashwinirajashekar1111@gmail.com>
2023-11-23 17:18:25 +0530

    Project-2

---
+++ ConsoleAppProject-2/ConsoleAppProject-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"C:\Mphasis -B233\Project-2\Student Info .text";
+
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            Console.WriteLine("File not found. Please check the file path.");
+        }
+    }
+}
